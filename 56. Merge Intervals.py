93842468faeca009a63cceef509596a79aa9d268
Yogesh Sehgal <yogeshsehgal99@rediff.com>
2020-09-19 23:04:43 +0530

    Day 1

--- 56. Merge Intervals.py
+++ 56. Merge Intervals.py
@@ -13,3 +13,19 @@ class Solution:
             else:
                 merged[-1][1] = max(merged[-1][1], interval[1])
         return merged
+
+# Partial Solution. (Brute Force)
+
+    class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        first = 0
+        second = 1
+        intervals.sort()
+        while second < len(intervals):
+            if intervals[first][1] >= intervals[second][0]:
+                intervals[first][1] = max(
+                    intervals[second][1], intervals[first][1])
+                intervals.pop(second)
+            first += 1
+            second += 1
+        return intervals
