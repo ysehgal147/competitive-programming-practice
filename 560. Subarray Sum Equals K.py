5c80f8cc95ca00aaeca8503866dacf9167a8e292
Yogesh Sehgal <yogeshsehgal99@rediff.com>
2020-09-24 21:13:12 +0530

    DAY 4

---
+++ 560. Subarray Sum Equals K.py
@@ -0,0 +1,17 @@
+# 560. Subarray Sum Equals K
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+
+        #Solution 1. (Using Hash Table)
+
+        count, p, d = 0, 0, {0: 1}
+        for i in nums:
+            p += i
+            if p-k in d:
+                count += d[p-k]
+            if p not in d:
+                d[p] = 1
+            else:
+                d[p] += 1
+        return count
